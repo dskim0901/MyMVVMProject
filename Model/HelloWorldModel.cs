de512d1d5df2e11c7813abf1b2bb1c78e8487e0b
김동수 <dskim@jltech.co.kr>
2022-06-11 20:38:26 +0900

    source 추가

--- Model/HelloWorldModel.cs
+++ Model/HelloWorldModel.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace MyMVVMProject.Model
 {
-    internal class HelloWorldModel
+    public class HelloWorldModel
     {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+        /// <summary>
+        /// Simulates data retrieval from a repository
+        /// </summary>
+        /// <returns>List of strings</returns>
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "world"
+            };
+            return repositoryData;
+        }
+
+        /// <summary>
+        /// Concatenate the information from the list into a fully formed sentence.
+        /// </summary>
+        /// <returns>A string</returns>
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
     }
 }
