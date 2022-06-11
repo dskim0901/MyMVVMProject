05f3b121727ce631685863aa12f440c6ead8378b
김동수 <dskim@jltech.co.kr>
2022-06-11 20:33:21 +0900

    기본 작업 Create MVVM folder Create Basic File

--- View/HelloWorldView.xaml.cs
+++ View/HelloWorldView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyMVVMProject
+namespace MyMVVMProject.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HelloWorldView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HelloWorldView : Window
     {
-        public MainWindow()
+        public HelloWorldView()
         {
             InitializeComponent();
         }
